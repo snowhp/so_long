ce5e086b8d56655d4d3186663790198ca6e80889
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-04-08 21:10:38 +0100

    Includes added

--- inc/solong.h
+++ inc/solong.h
@@ -6,7 +6,15 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 21:01:23 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/04/08 21:01:23 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/04/08 21:08:16 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "../libft/libft.h"
