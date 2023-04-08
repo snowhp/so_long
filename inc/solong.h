a4090666f31914ef92877d7eefa5982a600d6457
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-04-08 22:50:27 +0100

    Struct added and define

--- inc/solong.h
+++ inc/solong.h
@@ -6,15 +6,28 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 21:01:23 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/04/08 21:08:16 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/04/08 22:49:39 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include "../libft/libft.h"
+#ifndef PIPEX_H
+# define PIPEX_H
+
+# include <fcntl.h>
+# include <sys/stat.h>
+# include <sys/types.h>
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <errno.h>
+# include "../libft/libft.h"
+
+typedef struct s_data
+{
+	int		FILLL;
+}	t_data;
+
+void    ft_checkmap(char *str)
+
+#endif
