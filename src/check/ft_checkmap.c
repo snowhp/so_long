ace13133a035f2bf6b7a613b6699a852f25b5168
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-04-08 21:11:02 +0100

    Includes added

--- src/check/ft_checkmap.c
+++ src/check/ft_checkmap.c
@@ -1,21 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_checkmap.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/08 21:01:20 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/04/08 21:09:19 by tde-sous         ###   ########.fr       */
+/*   Created: 2023/04/08 21:04:00 by tde-sous          #+#    #+#             */
+/*   Updated: 2023/04/08 21:09:46 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../inc/solong.h"
+#include "../../inc/solong.h"
 
-int	main(int	argc, char** argv)
+void    ft_checkmap(char *str)
 {
-	if(argc != 2)
-		exit (1);
-	ft_checkmap();
-	/* Initialize struct*/
+
 }
