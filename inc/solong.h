cdd59293d934c840044720edab9dd27b1e450595
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-04-09 09:35:04 +0100

    Exit function and includes

--- inc/solong.h
+++ inc/solong.h
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 21:01:23 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/04/08 22:49:39 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/04/09 09:34:47 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,6 +21,9 @@
 # include <stdio.h>
 # include <string.h>
 # include <errno.h>
+# include <X11/X.h>
+# include <X11/keysym.h>
+# include <mlx.h>
 # include "../libft/libft.h"
 
 typedef struct s_data
@@ -28,6 +31,7 @@ typedef struct s_data
 	int		FILLL;
 }	t_data;
 
-void    ft_checkmap(char *str)
+void    ft_checkmap(char *str);
+void    ft_exit(char	*str, int i);
 
 #endif
--- src/error/errors.c
+++ src/error/errors.c
@@ -1,33 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_checkmap.c                                      :+:      :+:    :+:   */
+/*   errors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/08 21:04:00 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/04/09 09:33:02 by tde-sous         ###   ########.fr       */
+/*   Created: 2023/04/09 09:31:38 by tde-sous          #+#    #+#             */
+/*   Updated: 2023/04/09 09:34:22 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/solong.h"
 
-void    ft_checkmap(char *str)
+void    ft_exit(char	*str, int i)
 {
-    int	i;
-
-    i = ft_strlen(str) - 4;
-	if (ft_strncmp(".ber", (str + i), 4) != 0)
-		ft_exit("Wrong extension of the file.", EXIT_FAILURE);
-	if (open(str, O_RDONLY, 0444) == -1)
-		ft_exit("Unable to open the map file.", EXIT_FAILURE );
-}
-
-int	main()
-{
-	ft_checkmap("asdasd.map.ber");
-	ft_checkmap("mapbeasdr");
-	ft_checkmap("map.bas.der");
-	ft_checkmap("map..be.ra");
-	return 0;
+	if(i == EXIT_SUCCESS)
+		exit(EXIT_SUCCESS);
+	else
+		ft_printf("Errorn\n %s", str);
+	//free anymemory
 }
