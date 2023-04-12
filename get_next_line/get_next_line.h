1e2ecf18c069190e6a660f34e80e66aea56a2439
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-04-12 15:06:33 +0100

    File updated

--- get_next_line/get_next_line.h
+++ get_next_line/get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tde-sous <tde-sous@42.porto.com>           +#+  +:+       +#+        */
+/*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/01 15:10:22 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/01/16 13:25:50 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/04/12 15:05:53 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,8 +24,14 @@
 #  define ARRAY_MAX_SIZE 4096
 # endif
 
+/**
+ * @brief This function requires a filedescriptor and return the next
+ * line.
+ * 
+ * @param fd File Descriptor 
+ * @return Next line in the file or NULL.
+ */
 char	*get_next_line(int fd);
-/* 	UTILS FT */
 char	*ft_strchr(char *str, int chr);
 char	*ft_strjoin(char *s1, char *s2);
 void	*ft_calloc(size_t nmemb, size_t size);
