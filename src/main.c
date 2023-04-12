1c836a3f8ed3b47f1786c625f597ab66702a9410
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-04-12 16:50:11 +0100

    Proper exit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 21:01:20 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/04/12 16:08:36 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/04/12 16:35:43 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	main(int	argc, char**	argv)
 	t_data	data;
 
 	if (argc != 2)
-		exit (1);
+		ft_exit("Usage: ./so_long MAP_PATH", EXIT_FAILURE, &data);
 	ft_init_data(&data);
 	ft_checkfile(argv[1], &data);
 	ft_loadmap(&data);
