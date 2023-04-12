6158e96bbdda196feec5256345e372bce1adf158
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-04-12 16:49:34 +0100

    Spelling fix

--- src/error/errors.c
+++ src/error/errors.c
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 09:31:38 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/04/12 16:02:07 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/04/12 16:36:45 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,6 @@ void	ft_exit(char	*str, int i, t_data *data)
 		free(data->map);
 	if (i == EXIT_SUCCESS)
 		exit(EXIT_SUCCESS);
-	ft_printf("Errorn\n %s", str);
+	ft_printf("Error\n %s", str);
 	exit(EXIT_FAILURE);
 }
