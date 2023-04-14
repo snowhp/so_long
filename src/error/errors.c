289bacfb6e804d183b95683a80bde84a4e157994
Tiago Machado <13867481+snowhp@users.noreply.github.com>
2023-04-14 01:11:21 +0100

    Seg fault fix when ft_exit used without map created

--- src/error/errors.c
+++ src/error/errors.c
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/09 09:31:38 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/04/13 15:21:49 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/04/14 01:05:09 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,13 @@ void	ft_exit(char	*str, int i, t_data *data)
 	/* free anymemory */
 	close(data->fd);/* Should be close after no longer needed prob ft_checkmap.c*/
 	y = 0;
-	while (data->map[y])
-		free(data->map[y++]);
-	if (data->map)
-		free(data->map);
+	if(data->map)
+	{
+		while (data->map[y])
+			free(data->map[y++]);
+		if (data->map)
+			free(data->map);
+	}
 	if (i == EXIT_SUCCESS)
 		exit(EXIT_SUCCESS);
 	ft_printf("Error\n%s", str);
