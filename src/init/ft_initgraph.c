2a4cabe7ddf65301e79f2dd155ee3f4c34ea31a0
SnowHP <13867481+snowhp@users.noreply.github.com>
2023-04-17 11:55:28 +0100

    Window size fixed

--- src/init/ft_initgraph.c
+++ src/init/ft_initgraph.c
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 03:09:44 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/04/17 11:37:13 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/04/17 11:51:57 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	ft_initgraph(t_data *data)
 	data->mlx_ptr = mlx_init();
 	if (!data->mlx_ptr)
 		ft_exit("Failed to initialize window.", EXIT_FAILURE, data);
-	data->mlx_win = mlx_new_window(data->mlx_ptr, data->max_y * SIZE, data->max_y * SIZE, "so_long 42" );
+	data->mlx_win = mlx_new_window(data->mlx_ptr, data->max_x * SIZE, data->max_y * SIZE, "so_long 42" );
 	if (!data->mlx_win)
 	{
 		mlx_destroy_display(data->mlx_ptr);
