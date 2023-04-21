6df51b5cbb8541887b9ce8124c89ef2f8e8b05cc
SnowHP <13867481+snowhp@users.noreply.github.com>
2023-04-21 14:34:57 +0100

    Fix: Crash on no wall in max_x

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: tde-sous <tde-sous@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 21:01:20 by tde-sous          #+#    #+#             */
-/*   Updated: 2023/04/21 12:49:15 by tde-sous         ###   ########.fr       */
+/*   Updated: 2023/04/21 14:24:52 by tde-sous         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,8 +22,8 @@ int	main(int argc, char **argv)
 	ft_init_data(&data);
 	ft_checkfile(argv[1], &data);
 	ft_loadmap(argv[1], &data);
-	ft_checkmap(&data);
 	ft_mapformat(&data);
+	ft_checkmap(&data);
 	ft_mapflood(&data);
 	y = 0;
 	if (data.map)
